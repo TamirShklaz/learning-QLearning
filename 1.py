ff6e98de5199945559c9ff60b7da29c51c64e41f
Tamir Shklaz <shklazt@gmail.com>
2019-09-27 16:42:29 +0200

    Q Learning Implemented

--- 1.py
+++ 1.py
@@ -1,37 +1,73 @@
 import gym
+import numpy as np
 
 env = gym.make("MountainCar-v0")
 env.reset()
 
-# High state -> desired state
-print(env.observation_space.high)
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPISODES = 25000
 
-import numpy as np
+SHOW_EVERY = 2000
 
-# Low state
-print(env.observation_space.low)
+# the chance that we try a random action
+epsilon = 0.5
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES // 2
 
-# Number of actions
-print(env.action_space.n)
+epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
+
+
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low) / discrete_os_win_size
+    return tuple(discrete_state.astype(np.int))
+
 
 # We need to discretise the observations because they are too many
 # So we are creating 20 discrete values from a continous data set
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)  # 20x20
 
-#step size between samples
+# step size between samples
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
-print(discrete_os_win_size)
 
-#low is reward of every state
-#high is reward for goal state
-#Make a 20x20x3 table (position, velocity, action)
+# low is reward of every state
+# high is reward for goal state
+# Make a 20x20x3 table (position, velocity, action)
+# Q(s,a) = V s -> (p,v)
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
-# done = False
-#
-# while not done:
-#     action = 2
-#     new_state, reward, done, _ = env.step(action)
-#     print(new_state)
-#     env.render()
-#
-# env.close()
+
+render = False
+for episode in range(EPISODES):
+    done = False
+    discrete_state = get_discrete_state(env.reset())
+
+    if episode % SHOW_EVERY == 0:
+        render = True
+    else:
+        render = False
+    while not done:
+        # argmax returns the index of the biggest element
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[discrete_state])
+        else:
+            action = np.random.randint(0, env.action_space.n)
+
+        new_state, reward, done, _ = env.step(action)
+        new_discrete_state = get_discrete_state(new_state)
+        if render:
+            env.render()
+        if not done:
+            max_future_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state][action]
+            # Reward is always -1 except the end
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            q_table[discrete_state][action] = new_q
+        elif new_state[0] >= env.goal_position:
+            print("Found on", episode)
+            q_table[discrete_state][action] = 0
+        discrete_state = new_discrete_state
+
+    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+        epsilon -= epsilon_decay_value
+
+env.close()
